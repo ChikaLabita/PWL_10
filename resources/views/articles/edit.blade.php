4500fec6d0336e75d99df7458a83b58c1d924c2b
= <=>
2021-04-13 22:52:09 +0700

    PWL-10 : Revisi edit.blade.php

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -12,13 +12,13 @@
 </div>
 <div class="form-group">
 <label for="content">Content</label>
-<input type="text" class="form-control" required="required" name="content" value="{{$article>content}}"></br>
+<input type="text" class="form-control" required="required" name="content" value="{{$article->content}}"></br>
 </div>
 <div class="form-group">
 <label for="image">Feature Image</label>
-<input type="file" class="form-control" required="required" name="image" value="{{$article>featured_image}}"></br>
+<input type="file" class="form-control" required="required" name="image" value="{{$article->featured_image}}"></br>
 <!--Menampilkan gambar pada database dengan max lebar = 150px-->
-<img width="150px" src="{{asset('storage/'.$article>featured_image)}}">
+<img width="150px" src="{{asset('storage/'.$article->featured_image)}}">
 </div>
 <button type="submit" class="btn btn-primary float-right">Ubah Data</button>
 </form>
