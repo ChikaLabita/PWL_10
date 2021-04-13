b84b3a0ff8f159883baa8db189b112edad667125
= <=>
2021-04-13 17:43:30 +0700

    PWL-10 : Create Auth

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 });*/
 
 Route::resource('articles', ArticleController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
