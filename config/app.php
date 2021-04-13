c84a52aaf663c3a59d17fe28af7fa86162e45bda
= <=>
2021-04-13 22:50:43 +0700

    PWL-10 : Revisi Peletakan code -> providers + aliases (config->app.php)

--- config/app.php
+++ config/app.php
@@ -1,16 +1,6 @@
 <?php
 
 return [
-
-    'providers' => [
-       //....
-        Barryvdh\DomPDF\ServiceProvider::class
-    ],
-    'aliases' => [
-        //....
-        'PDF' => Barryvdh\DomPDF\Facade::class
-    ],
-
     /*
     |--------------------------------------------------------------------------
     | Application Name
@@ -170,6 +160,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -236,6 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class
 
     ],
 
